722f4e22bc939db37b70d034388caedf3b3be47f
CarlinChow <carlin_chow@sfu.ca>
2023-12-26 10:40:43 -0800

    renamed package

--- src/test/java/com/carlinchow/expenseTracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/carlinchow/expenseTracker/ExpenseTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.carlinchow.expenseTracker.expenseTracker;
+package com.carlinchow.expenseTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
