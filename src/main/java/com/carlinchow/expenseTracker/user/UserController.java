b49c4d385dfed68118040f67e378276267a40758
CarlinChow <carlin_chow@sfu.ca>
2023-12-27 17:27:47 -0800

    added user feature

--- src/main/java/com/carlinchow/expenseTracker/user/UserController.java
+++ src/main/java/com/carlinchow/expenseTracker/user/UserController.java
@@ -1,25 +1,32 @@
 package com.carlinchow.expenseTracker.user;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api/user")
 public class UserController {
-    private UserRepository userRepository;
+    private final UserService service;
 
     @Autowired
-    public UserController(UserRepository userRepository){
-        this.userRepository = userRepository;
+    public UserController(UserService service) {
+        this.service = service;
     }
 
     @GetMapping
     public List<User> getUsers(){
-        return userRepository.findAll();
+        return this.service.getUsers();
+    }
+
+    @PostMapping
+    public void adduser(@RequestBody User user){
+        this.service.addUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long id){
+        this.service.deleteUser(id);
     }
 }
--- src/main/java/com/carlinchow/expenseTracker/user/UserRepository.java
+++ src/main/java/com/carlinchow/expenseTracker/user/UserRepository.java
@@ -1,9 +1,14 @@
 package com.carlinchow.expenseTracker.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<User> findOneByEmail(String email);
 }
