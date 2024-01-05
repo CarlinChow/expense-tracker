bed26e0f23febffefd358a54b51ea5fa2d9de6b7
CarlinChow <carlin_chow@sfu.ca>
2024-01-04 17:22:13 -0800

    added repositories

--- src/main/java/com/carlinchow/expenseTracker/transaction/Expense/Expense.java
+++ src/main/java/com/carlinchow/expenseTracker/transaction/Expense/Expense.java
@@ -2,30 +2,17 @@ package com.carlinchow.expenseTracker.transaction.Expense;
 
 import com.carlinchow.expenseTracker.category.Category;
 import com.carlinchow.expenseTracker.transaction.Transaction;
-import com.carlinchow.expenseTracker.user.User;
 import jakarta.persistence.*;
-import lombok.Builder;
-
-import java.time.LocalDate;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue("EXPENSE")
-@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 public class Expense extends Transaction {
     private @ManyToOne @JoinColumn(name="category_id") Category category;
-
-    public Expense() { super(); }
-
-    public Expense(LocalDate date, Float amount, String description, User user, Category category) {
-        super(date, amount, description, user);
-        this.category = category;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 }
---
+++ src/main/java/com/carlinchow/expenseTracker/transaction/Income/IncomeRepository.java
@@ -0,0 +1,9 @@
+package com.carlinchow.expenseTracker.transaction.Income;
+
+import com.carlinchow.expenseTracker.transaction.TransactionBaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IncomeRepository extends TransactionBaseRepository<Income> {
+
+}
